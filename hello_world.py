b51b4dde19715faec478273096803d4f8c117c93
Dillan <Dillan.Pietersen@sasol.com>
2026-08-04 10:26:26 +0200

    Changes have been successfully committed !

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# DP26060020553 M03T06 – GIT Version Control & Basics
+# Dillan Pietersen
+
+# This is a simple Python program that prompts the user for their name and age, and then prints a greeting message.
+
+user_name = input("Please enter your full name: ")
+print(user_name)
+
+user_age = input("Please enter your age: ")
+
+print("Hello, World!")
+print(user_name, "you are", str(user_age), "years old")
